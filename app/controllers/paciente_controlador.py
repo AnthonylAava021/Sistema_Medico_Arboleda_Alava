dd3e6779919be3dd43304c422efe7a71dcd8234d
Anthony Joseph Álava Reyes <anthony_al05@outlook.com>
2025-06-26 22:20:28 -0500

    Update paciente_controlador.py

---
+++ app/controllers/paciente_controlador.py
@@ -0,0 +1,24 @@
+from flask_restx import Namespace, Resource, fields
+from app.services.paciente_servicio import PacienteService
+
+api = Namespace('pacientes', description='Operaciones con pacientes')
+#Se establece el modelo que tendrá los pacientes
+modelo_paciente = api.model('Paciente', {
+    'nombre': fields.String(required=True),
+    'apellido': fields.String(required=True),
+    'cedula': fields.String(required=True),
+    'telefono': fields.String,
+    'correo': fields.String
+})
+#Asignación de las rutas o endpoints
+@api.route('/')
+class PacienteList(Resource):
+
+    @api.marshal_list_with(modelo_paciente)
+    def get(self):
+        return PacienteService.listar()
+
+    @api.expect(modelo_paciente)
+    @api.marshal_with(modelo_paciente, code=201)
+    def post(self):
+        return PacienteService.crear(api.payload), 201
