4ab971fd1d44eaeb34412902093c0318340e5ce7
Anthony Joseph Álava Reyes <anthony_al05@outlook.com>
2025-06-26 21:46:18 -0500

    Iniicialización de la base de datos

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restx import Api
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('app.config.Config')
+
+    db.init_app(app)
+
+    api = Api(app, version='1.0', title='API Citas Médicas', description='Sistema de Gestión de Citas Médicas', doc='/docs')
+
+    # Importar namespaces
+    from app.controllers.paciente_controller import api as paciente_ns
+    from app.controllers.cita_controller import api as cita_ns
+
+    api.add_namespace(paciente_ns)
+    api.add_namespace(cita_ns)
+
+    return app
